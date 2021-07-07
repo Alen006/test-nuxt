0b4742d2acaf38be6e58299959ae7e195a702b30
Alen Jakob <a.jakob@tim.pl>
2021-07-07 14:22:38 +0200

    feature/first push

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    h1.title Welcome
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.title
+  color brown
+  border 1px solid black
+  padding 1em
+</style>
